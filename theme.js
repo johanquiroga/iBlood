7ab8c6d5176d7d8fcb8d21c79a0b5527374e0576
Johan Quiroga <johan.c.quiroga@gmail.com>
2019-08-25 19:26:23 -0500

    Initial commit: initialized app with expo and added necessary assets and added initial global styles

---
+++ theme.js
@@ -0,0 +1,53 @@
+const colors = {
+  accent: '#f72b2b',
+  primary: '#d61b1f',
+  secondary: '#3a3232',
+  tertiary: '#ed6004',
+  black: '',
+  white: '',
+  gray: '#d9d2d2',
+  gray2: '#f6f5f5',
+};
+
+const sizes = {
+  // global sizes
+  base: 12,
+  font: 12,
+  border: 12,
+
+  // font sizes
+  h1: 32,
+  h2: 26,
+  h3: 18,
+  title: 16,
+  body: 12,
+  caption: 12,
+  small: 8,
+};
+
+const fonts = {
+  h1: {
+    // fontFamily: "Montserrat-Bold",
+    fontSize: sizes.h1,
+  },
+  h2: {
+    fontSize: sizes.h2,
+  },
+  h3: {
+    fontSize: sizes.h3,
+  },
+  title: {
+    fontSize: sizes.title,
+  },
+  body: {
+    fontSize: sizes.body,
+  },
+  caption: {
+    fontSize: sizes.caption,
+  },
+  small: {
+    fontSize: sizes.small,
+  },
+};
+
+export { colors, sizes, fonts };
